f2f87e7c98fbed938954daac2615d6629eeb78fa
王誉 <wangyu@yunjiacloud.com>
2022-05-27 16:57:55 +0800

    1.新碟上架功能完善

--- src/components/index/latestmusic/newalbum.vue
+++ src/components/index/latestmusic/newalbum.vue
@@ -2,74 +2,111 @@
     <div class="wrap-newalbum">
         <div class="newalbum-option-btn">
             <div class="option-list">
-                <span v-for="(item,index) in optionList" :class="[optionType == item.data ? 'on' : '']" :key="index" @click="changeOption(item.data)">{{item.name}}</span>
+                <span v-for="(item,index) in optionList" :class="[optionarea == item.data ? 'on' : '']" :key="index" @click="changearea(item.data)">{{item.name}}</span>
             </div>
-            <div class="btn-a clear">
-                
-            </div>
+            <!-- <div class="btn-a clear">
+                <span @click="changeType('hot')">推荐</span>
+                <span @click="changeType('new')">全部</span>
+            </div> -->
         </div>
-        <div class="newalbum-section">
+        <div class="newalbum-section" v-if="albumList.length > 0">
             <div class="newalbum-list clear" v-for="(item,index) in albumList" :key="index">
                 <div class="newalbum-left fl">
-                    <div :class="[albumOn == index ? 'on' : '']">{{item.timedate == 'weekData' ? "<span>本周</span>" : item.timedate}}</div>
+                    <div :class="['newalbum-fixed',albumOn == index ? 'on' : '']">
+                        <div class="weekData" v-if="item.timedate == 'weekData'">
+                            <span>本周新碟</span>
+                        </div>
+                        <div class="beforedate" v-else>
+                            <div class="month">
+                                {{item.timedate.split('-')[1]}}
+                            </div>
+                            <div class="year">
+                                {{item.timedate.split('-')[0]}}
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="newalbum-right clear fl">
                     <div class="newalbum-cover fl" v-for="item2 in item.data" :key="item2.id">
-                        <div class="newalbum-img">
-                            <img :src="item2.picUrl + '?param=300y300'"/>
+                        <div class="newalbum-img" @click="goPage(router,'/album',{id:item2.id})">
+                            <img v-lazy="item2.picUrl + '?param=300y300'"/>
+                            <div class="newalbum-playbtn amn4"></div>
                         </div>
+                        <div class="newalbum-name" @click="goPage(router,'/album',{id:item2.id})">{{item2.name}}</div>
+                        <div class="newalbum-artist" @click="goPage(router,'/artist',{id:item2.artist.id})">{{item2.artist.name}}</div>
                     </div>
                 </div>
             </div>
         </div>
+        <LoadingCpn v-else/>
     </div>
 </template>
 
 <script>
-import { defineComponent,onMounted,reactive,toRefs } from 'vue'
+import { defineComponent,onMounted,reactive,toRefs,inject,onBeforeUnmount } from 'vue'
 import { postJson } from '@/api/apiConfig'
 import { gettopalbum } from '@/api/api'
-import { addDate2,myDate } from '@/utils/common'
+import { addDate2,myDate,goPage } from '@/utils/common'
+import { useRouter } from 'vue-router'
+import LoadingCpn from "@/components/common/loadingcpn.vue"
 export default defineComponent({
     name:'handtailor',
+    components:{
+        LoadingCpn
+    },
     setup () {
         let state = reactive({
             optionList:[
                 {
                     name: '全部',
-                    data: 0,
+                    data: 'ALL',
                 },
                 {
                     name: '华语',
-                    data: 7,
+                    data: 'ZH',
                 },
                 {
                     name: '欧美',
-                    data: 96,
+                    data: 'EA',
                 },
                 {
                     name: '日本',
-                    data: 8,
+                    data: 'JP',
                 },
                 {
                     name: '韩国',
-                    data: 16,
+                    data: 'KR',
                 }
             ],
-            optionType:0,
+            optionarea:'ALL',
             albumList:[],
             albumOn:-1,
             today: myDate(),
             timeDown:0,
+            loading:false,
+            optionType:'new'
         })
-        let changeOption = (data) => {
+        let newalbumLeftData = inject('newalbumLeftData')
+        let changearea = (data) => {
+            state.optionarea = data
+            state.albumList = []
+            state.albumOn = -1
+            state.timeDown = 0
+            getData()
+        }
+        let changeType = (data) => {
             state.optionType = data
+            state.albumList = []
+            state.albumOn = -1
+            state.timeDown = 0
             getData()
         }
         let getData = () => {
             let year = addDate2(state.today,state.timeDown).split('-')[0]
             let month = addDate2(state.today,state.timeDown).split('-')[1]
-            postJson(gettopalbum,{year:year,month:month},res => {
+            state.loading = true
+            postJson(gettopalbum,{year:year,month:month,area:state.optionarea,type:state.optionType},res => {
+                state.loading = false
                 if(res.code == 200){
                     let weekobj = {},obj = {}
                     if(res.weekData && res.weekData.length > 0){
@@ -82,8 +119,6 @@ export default defineComponent({
                         obj.data = res.monthData
                         state.albumList.push(obj)
                     }
-                    // state.albumList.weekData = res.weekData
-                    // state.albumList.monthData = res.monthData
                 }
             },err => {
 
@@ -92,36 +127,51 @@ export default defineComponent({
         onMounted(() => {
             const el = document.querySelector(".index-screen")
             const offsetHeight = el.offsetHeight
-            const albumDatedom = document.getElementsByClassName("newalbum-left")
             el.onscroll = () => {
+                const albumDatedom = document.getElementsByClassName("newalbum-left")
                 Array.prototype.forEach.call(albumDatedom,function(el,index){
-                    let dom = el.getBoundingClientRect()
-                    console.log(dom)
-                    if(dom.top < 100){
+                    let domRect = el.getBoundingClientRect()
+                    // console.log(dom)
+                    if(domRect.top < 200){
                         state.albumOn = index
+                        newalbumLeftData.newalbumOn = true
+                        if(index == 0){
+                            newalbumLeftData.timedate = 'weekData'
+                        }else{
+                            newalbumLeftData.timedate = el.getElementsByClassName('year')[0].innerHTML + '-' + el.getElementsByClassName('month')[0].innerHTML
+                        }
                     }
-                    if(dom.top > 110 && index == 0){
+                    if(domRect.top > 110 && index == 0){
                         state.albumOn = -1
+                        newalbumLeftData.newalbumOn = false
                     }
                 })
                 const scrollTop = el.scrollTop
                 const scrollHeight = el.scrollHeight
-                if(offsetHeight + scrollTop - scrollHeight >= 0){
+                if(offsetHeight + scrollTop - scrollHeight >= -10000 && !state.loading){
                     state.timeDown--
                     getData()
                 }
             }
         })
+        onBeforeUnmount(() => {
+            newalbumLeftData.newalbumOn = false
+        })
         getData()
+        const router = useRouter()
         return {
             ...toRefs(state),
-            changeOption
+            changearea,
+            router,
+            goPage,
+            changeType
         }
     }
 })
 </script>
 <style scoped lang="scss">
 .wrap-newalbum{
+    padding-bottom: 20px;
     .newalbum-option-btn{
         padding-top: 26px;
         display: flex;
@@ -144,6 +194,22 @@ export default defineComponent({
                 color: $font-hovercolor;
             }
         }
+        .btn-a{
+            span{
+                font-size: 13px;
+                display: inline-block;
+                color: $font-authorcolor;
+                cursor: pointer;
+                margin: 0 20px;
+            }
+            span:hover{
+                color: $font-hovercolor;
+            }
+            span:last-child{
+                margin-right: 0;
+                border-width: 0;
+            }
+        }
     }
     .newalbum-section{
         .newalbum-list{
@@ -152,16 +218,92 @@ export default defineComponent({
                 box-sizing: border-box;
                 font-size: 18px;
                 padding: 0 12px 0 2px;
-                height: 50px;
-            }
-            .newalbum-left.on{
-                position: fixed;
-                margin: -100px 0 0 0;
+                min-height: 50px;
+                .newalbum-fixed{
+                    .month{
+                        width: 38px;
+                        height: 38px;
+                        background: url('../../../assets/img/datebg.png') center no-repeat;
+                        background-size:100% 100%;
+                        text-align: center;
+                        line-height: 48px;
+                    }
+                    .year{
+                        font-size: 12px;
+                        padding-left: 6px;
+                        padding-top: 3px;
+                    }
+                }
+                .newalbum-fixed.on{
+                    display:none;
+                }
             }
             .newalbum-right{
                 width: calc(100% - 50px);
                 .newalbum-cover{
-                    width: 25%;
+                    width: 22.5%;
+                    margin-right: 3.3333333%;
+                    margin-bottom: 18px;
+                    background: url('../../../assets/img/albumbg.png') center top no-repeat;
+                    background-size: 100%;
+                }
+                .newalbum-img{
+                    min-width: 135.2px;
+                    min-height: 135.2px;
+                    max-width: 85%;
+                    border-radius: 5px;
+                    overflow: hidden;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    cursor: pointer;
+                    position: relative;
+                    .newalbum-playbtn{
+                        position: absolute;
+                        width: 36px;
+                        height: 36px;
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        right: 0;
+                        bottom: 0;
+                        margin: auto;
+                        background: url('../../../assets/img/player-daily.png') center no-repeat;
+                        background-size: cover;
+                        opacity:0;
+                    }
+                }
+                .newalbum-img:hover{
+                    .newalbum-playbtn{
+                        opacity: 1;
+                    }
+                }
+                .newalbum-cover:nth-child(4n){
+                    margin-right: 0;
+                }
+                .newalbum-name{
+                    font-size: 14px;
+                    margin: 10px 0;
+                    display: -webkit-box;
+                    -webkit-box-orient: vertical;
+                    -webkit-line-clamp: 1;
+                    overflow: hidden;
+                    cursor: pointer;
+                }
+                .newalbum-name:hover{
+                    color: $font-hovercolor;
+                }
+                .newalbum-artist{
+                    font-size: 12px;
+                    color:$font-authorcolor;
+                    display: -webkit-box;
+                    -webkit-box-orient: vertical;
+                    -webkit-line-clamp: 1;
+                    overflow: hidden;
+                    cursor: pointer;
+                }
+                .newalbum-artist:hover{
+                    color:$font-hoverauthorcolor;
                 }
             }
         }
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,18 +2,41 @@
   <div class="wrap-index">
     <IndexNav/>
     <div class="index-screen">
+      <div class="newalbum-fixed" v-show="newalbumOn"><!--发现音乐/最新音乐  新碟上架相关功能-->
+        <div class="weekData" v-if="timedate == 'weekData'">
+          <span>本周新碟</span>
+        </div>
+        <div class="beforedate" v-else>
+          <div class="month">
+            {{timedate.split('-')[1]}}
+          </div>
+          <div class="year">
+            {{timedate.split('-')[0]}}
+          </div>
+        </div>
+      </div>
       <router-view />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent,reactive,toRefs,provide } from 'vue'
 import IndexNav from '@/components/index/IndexNav.vue'
 export default defineComponent({
   name:'Index',
   components:{
     IndexNav
+  },
+  setup () {
+    let state = reactive({
+      newalbumOn:false,
+      timedate:''
+    })
+    provide('newalbumLeftData',state)
+    return {
+      ...toRefs(state),
+    }
   }
 })
 </script>
@@ -27,6 +50,25 @@ export default defineComponent({
     padding: 0 30px;
     overflow-y: scroll;
     box-sizing: border-box;
+    .newalbum-fixed{
+      position: absolute;
+      width: 50px;
+      font-size: 18px;
+      margin-left: 2px;
+      .month{
+        width: 38px;
+        height: 38px;
+        background: url('../assets/img/datebg.png') center no-repeat;
+        background-size:100% 100%;
+        text-align: center;
+        line-height: 48px;
+      }
+      .year{
+        font-size: 12px;
+        padding-left: 6px;
+        padding-top: 3px;
+      }
+    }
   }
 }
 </style>
